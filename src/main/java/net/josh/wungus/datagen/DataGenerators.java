2694eac9368fdcb23c31b4684c6b58de37c11cd5
Josh Gottschall <josh@jgottschall.com>
2025-04-12 11:31:57 -0400

    Added back spawns and advancements

--- src/main/java/net/josh/wungus/datagen/DataGenerators.java
+++ src/main/java/net/josh/wungus/datagen/DataGenerators.java
@@ -36,10 +36,10 @@ public class DataGenerators {
         //generator.addProvider(event.includeServer(), new ModGlobalLootModifiersProvider(packOutput));
         //generator.addProvider(event.includeServer(), new ModPoiTypeTagsProvider(packOutput, lookupProvider, existingFileHelper));
 
-        //generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
 
         generator.addProvider(event.includeServer(), new ModGlobalLootModifierProvider(packOutput));
 
-        //generator.addProvider(event.includeClient(), new ForgeAdvancementProvider(packOutput, lookupProvider, existingFileHelper, List.of(new ModAdvancementProvider())));
+        generator.addProvider(event.includeClient(), new ForgeAdvancementProvider(packOutput, lookupProvider, existingFileHelper, List.of(new ModAdvancementProvider())));
     }
 }
