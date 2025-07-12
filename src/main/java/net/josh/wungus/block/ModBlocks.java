c6428ee779f3073bcef25963cc9853c1bc1e52b6
Josh Gottschall <josh@jgottschall.com>
2025-07-12 18:52:27 -0400

    Added item model for quartz landmine
    Added wungus staute (texture pending)

--- src/main/java/net/josh/wungus/block/ModBlocks.java
+++ src/main/java/net/josh/wungus/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.josh.wungus.block;
 import net.josh.wungus.WungusMod;
 import net.josh.wungus.block.custom.QuartzLandmine;
 import net.josh.wungus.block.custom.WungusEgg;
+import net.josh.wungus.block.custom.WungusStatue;
 import net.josh.wungus.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -28,6 +29,9 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> QUARTZ_LANDMINE = registerBlock("quartz_landmine",
             () -> new QuartzLandmine(BlockBehaviour.Properties.copy(Blocks.QUARTZ_BLOCK).sound(SoundType.NETHER_ORE).randomTicks()));
+
+    public static final RegistryObject<Block> WUNGUS_STATUE = registerBlock("wungus_statue",
+            () -> new WungusStatue(BlockBehaviour.Properties.copy(Blocks.GLASS_PANE).sound(SoundType.STONE).randomTicks()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
